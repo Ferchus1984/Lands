ada62b7c79840a2fc62d9bdcb1326455831cccc3
FERNANDO MARTINEZ <monterrey_pc@hotmail.com>
2019-02-09 22:46:52 -0600

    login view models modificado de propiedades, Constructoers

--- lands/Lands/Lands/ViewModels/MainViewModel.cs
+++ lands/Lands/Lands/ViewModels/MainViewModel.cs
@@ -9,7 +9,23 @@ namespace Lands.ViewModels
 {
 	public class MainViewModel
 	{
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
 
-	
-	}
+        }
+        #endregion
+
+        #region
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+
+
+        #endregion
+
+    }
 }
